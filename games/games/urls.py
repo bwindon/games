4b69b61639f9ec1d27834495717d35992fd92338
bwindon <brad.windon@gmail.com>
2022-03-29 20:10:52 +1100

    v3 graphs -requirements.txt - removed utils

--- games/games/urls.py
+++ games/games/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('records/', include('records.urls')),
-    path('sales/', include('sales.urls')),
 ]
